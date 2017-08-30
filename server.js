0d38c7e56b8b163a1785c5245e33d117bf5b1ed6
Randi Mortensen <3884187@rts-365.dk>
2017-08-30 15:55:37 +0200

    update

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 //var figur = require('./modules/disney');
+//var beregn = require('./Modules/beregn');
 var beregn = require('./Modules/dato');
 //var bmi = require('./Modules/bmi');
+
+
 //console.log(figur.fullname()[0]);
-
 //console.log(figur.fullname()[1]);
-
 // console.log(figur.firstname + " " + figur.lastname);
-
 //console.log(figur.fullname());
-
 //console.log(beregn.sum(6, 8));
-
+//console.log(beregn.BMI(75, 180));
 console.log(beregn.dato);
 
-//console.log(beregn.BMI(75, 180));
